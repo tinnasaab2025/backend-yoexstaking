140392d7cf5e04fd7bf0030a6b8986906acb9f0d
Sanjay Dhundhara <sanjaydhundhara@SKD.local>
2025-07-05 17:18:45 +0530

    update

--- server/models/Assets.js
+++ server/models/Assets.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from '../DB/database.js';
+import { sequelize } from '../db/database.js';
 
 
 export const Asset = sequelize.define('Asset', {
